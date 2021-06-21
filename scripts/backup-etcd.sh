bee81ff8d5417e95e9edc5c6874ca6b44a8816dc
Jason hawthorne <jhawthor@me.com>
2021-06-21 04:51:15 -0500

    update

--- scripts/backup-etcd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir /etcd-backup
-ETCDCTL_API=3 etcdctl --endpoints=192.168.198.147:2379 --cacert /etc/kubernetes/pki/etcd/ca.crt --cert /etc/kubernetes/pki/etcd/server.crt --key /etc/kubernetes/pki/etcd/server.key snapshot save /etcd-backup/etcd-snapshot-latest.db
