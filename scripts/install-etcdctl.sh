af3195b42ebb0f690e8b03fbf333ba1582b80c58
Jason hawthorne <jhawthor@me.com>
2021-06-24 17:15:39 -0500

    updates

--- scripts/install-etcdctl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export RELEASE="3.3.13"
-wget https://github.com/etcd-io/etcd/releases/download/v${RELEASE}/etcd-v${RELEASE}-linux-amd64.tar.gz
-tar xvf etcd-v${RELEASE}-linux-amd64.tar.gz
-rm -f etcd-v${RELEASE}-linux-amd64.tar.gz
-cd etcd-v${RELEASE}-linux-amd64
-mv etcdctl /usr/local/bin
